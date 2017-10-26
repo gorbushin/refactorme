a61373999cb08604f555df5e0f984f0009564003
Dmitry Gorbushin <gorbushin@users.noreply.github.com>
2017-10-26 23:20:11 +1300

    Add files via upload

---
+++ Controllers/StrengthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace refactorme.Controllers
+{
+    public class StrengthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result(Models.StrengthData strengthData)
+        {
+            switch (strengthData.Gender.ToString().ToLower())
+            {
+                case "male":
+                    ViewData["Result"] = strengthData.WeightLifted * Wilks.CalculateWilks(strengthData.BodyWeight, true);
+                    break;
+                case "female":
+                    ViewData["Result"] = strengthData.WeightLifted * Wilks.CalculateWilks(strengthData.BodyWeight, false);
+                    break;
+                default:
+                    ViewData["Result"] = "Wilks formula doesn't work for Aliens :-)";
+                    break;
+            }
+
+            return View();
+        }
+    }
+}
