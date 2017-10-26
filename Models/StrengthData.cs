8e0855b5b4bfd83ad5fdbd363f43520d77ec0230
Dmitry Gorbushin <gorbushin@gmail.com>
2017-10-26 23:08:30 +1300

    First draft

---
+++ Models/StrengthData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace refactorme.Models
+{
+    public class StrengthData
+    {
+        public int WeightLifted { get; set; }
+
+        public int BodyWeight { get; set; }
+
+        public GenderOptions Gender { get; set; }
+    }
+
+    public enum GenderOptions
+    {
+        Male,
+        Female,
+        Other
+    }
+}
